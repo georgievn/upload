5a7e73cc0ea956c0126b883426d603d5d152cd36
georgievn <40321473+georgievn@users.noreply.github.com>
2018-06-25 11:32:58 +0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://cloudprogdb:hL4VWzWLQB3liHZABBug5M9cGTjIAfwsjn0QFYa9jSmiQldWD0Y0q4Knqqes4fWo6xBCEBdbolwKPRE39BHUzQ==@cloudprogdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
